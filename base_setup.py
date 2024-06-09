3bbb18e5af0eb7ac6c9e86176723cffe450206bb
FantasySilence <1735793009@qq.com>
2024-06-09 15:04:43 +0800

    添加了数据库恢复脚本

---
+++ base_setup.py
@@ -0,0 +1,11 @@
+# =============================================== #
+# @Author: Fantasy_Silence                        #
+# @Time: 2024-06-07                               #
+# @IDE: Visual Studio Code & PyCharm              #
+# @Python: 3.9.7                                  #
+# =============================================== #
+# @Description: 快速创建并向数据库中插入数据         #
+# =============================================== #
+from src.modules.backup.create_database import QuickRepair
+
+QuickRepair.restore()
