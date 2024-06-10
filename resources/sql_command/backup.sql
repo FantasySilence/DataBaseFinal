aa772eb00945a99b75fc0707f9615dadc9171798
FantasySilence <1735793009@qq.com>
2024-06-10 20:51:16 +0800

    修改了备份与回档的SQL命令

--- resources/sql_command/backup.sql
+++ resources/sql_command/backup.sql
@@ -1,13 +1,17 @@
-create database DataBaseFinal
+/* 备份 */
+create proc back_up @savepath nvarchar(max)
+as
+    backup database DataBaseFinal to disk = @savepath
+go;
 
--- 查看备份文件内容
-RESTORE FILELISTONLY
-FROM DISK = 'F:\2024-6-9 15-42.bak';
-
--- 恢复数据库
-RESTORE DATABASE DataBaseFinal
-FROM DISK = 'F:\2024-6-9 15-42.bak'
-WITH REPLACE, -- 覆盖现有数据库
-     MOVE 'DataBaseFinal' TO 'xxx\MSSQL16.MSSQLSERVER\MSSQL\DATA\DataBaseFinal.mdf', -- 数据文件的路径
-     MOVE 'DataBaseFinal_log' TO 'xxx\MSSQL16.MSSQLSERVER\MSSQL\DATA\DataBaseFinal_log.ldf'; -- 日志文件的路径
+/* 回档 */
+create proc restore_
+    @backup_path nvarchar(200), @data_path nvarchar(200),
+    @log_path nvarchar(200)
+as
+    restore database DataBaseFinal
+    from disk =  @backup_path with replace,
+    move 'DataBaseFinal' to @data_path,
+    move 'DataBaseFinal_log' to @log_path
+go;
      
